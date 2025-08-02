d623ec0213660d829482bb5056ad012761095e3a
zojize <jeffzo01@qq.com>
2025-08-01 19:43:12 -0500

    timeline max-h

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -156,7 +156,7 @@ defineExpose({
       <div class="i-carbon-restart" />
     </button>
   </div>
-  <div class="mt-2 p-2 b b-blue h-48">
+  <div class="mt-2 p-2 b b-blue max-h-48">
     <svg ref="timelineEl" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 10" class="h-full w-full">
       <symbol id="carbon-arrow-left" viewBox="0 0 32 32"><!-- Icon from Carbon by IBM - undefined --><path fill="currentColor" d="m14 26l1.41-1.41L7.83 17H28v-2H7.83l7.58-7.59L14 6L4 16z" /></symbol>
       <symbol id="carbon-arrow-up" viewBox="0 0 32 32"><!-- Icon from Carbon by IBM - undefined --><path fill="currentColor" d="M16 4L6 14l1.41 1.41L15 7.83V28h2V7.83l7.59 7.58L26 14z" /></symbol>
